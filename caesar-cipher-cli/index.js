371be1615f439a520911e5bc9a1d21315341df10
Anton Yasko <yasko.anton.92@gmail.com>
2020-03-27 11:48:28 +0300

    feat: implement enter the data from terminal

--- caesar-cipher-cli/index.js
+++ caesar-cipher-cli/index.js
@@ -10,19 +10,6 @@ program
   .option('-a, --action <action>', 'an action encode/decode')
   .parse(process.argv)
 
-const inputText = fs.readFileSync(program.input, 'utf-8') || ''
-const outputArrayOfChar = []
-
-const shift = Number(program.shift)
-
-inputText.split('').forEach(char => {
-  outputArrayOfChar.push(decoder(shift, program.action, char, englishAlphabet))
-})
-
-if (outputArrayOfChar.length === 0) outputArrayOfChar.push('ERROR_MESSAGE')
-
-fs.writeFileSync(program.output, outputArrayOfChar.join(''), 'utf-8')
-
 function decoder (shift, action, char, alphabet) {
   alphabet = action === 'decode' ? alphabet.split('').reverse().join('') : alphabet
 
@@ -33,3 +20,43 @@ function decoder (shift, action, char, alphabet) {
 
   return char === char.toUpperCase() ? alphabet[position].toUpperCase() : alphabet[position]
 }
+
+const outputArrayOfChar = []
+const shift = Number(program.shift.slice(0, 2))
+let inputText
+
+if (program.input) {
+  inputText = fs.readFileSync(program.input, 'utf-8')
+
+  inputText.split('').forEach(char => {
+    outputArrayOfChar.push(decoder(shift, program.action, char, englishAlphabet))
+  })
+
+  if (program.output) {
+    fs.writeFileSync(program.output, outputArrayOfChar.join(''), 'utf-8')
+  } else {
+    process.stdout.write(`Your ${program.action} data: ${outputArrayOfChar.join('')}`)
+  }
+} else {
+  process.stdin.setEncoding('utf8')
+  process.stdout.write(`Enter the data you need to ${program.action}: `)
+
+  process.stdin.on('readable', () => {
+    while ((inputText = process.stdin.read()) !== null) {
+      inputText.split('').forEach(char => {
+        outputArrayOfChar.push(decoder(shift, program.action, char, englishAlphabet))
+      })
+      if (program.output) {
+        fs.writeFileSync(program.output, outputArrayOfChar.join(''), 'utf-8')
+      } else {
+        process.stdout.write(`Your ${program.action} data: ${outputArrayOfChar.join('')}`)
+        process.stdout.write(`Enter the data you need to ${program.action}: `)
+      }
+      outputArrayOfChar.length = 0
+    }
+  })
+}
+
+// process.stdin.on('end', () => {
+//   process.stdout.write('end');
+// })
